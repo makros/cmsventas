d2fa258be1993f795b2fbc3439cb5caabb4b343a
makros <lorente.marcos@gmail.com>
2017-12-12 14:14:48 +0100

    Añadimos tabla para listar categorías

--- web/app/src/Views/Categoria/categoria.php
+++ web/app/src/Views/Categoria/categoria.php
@@ -1,11 +1,11 @@
-<?php include_once APP_PATH.'/Views/header.php' ?>
+<?php include_once APP_PATH . '/Views/header.php' ?>
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Page Header
-        <small>Optional description</small>
+        Categorías
+        <small>Listado de categorías para los productos</small>
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
@@ -15,13 +15,60 @@
 
     <!-- Main content -->
     <section class="content container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="box">
+            <div class="box-header with-border">
+              <h3 class="box-title pull-right">
+                <button class="btn btn-success" onclick="mostrarForm(true)"><i class="fa fa-plus-circle"></i> Agregar categoría </button>
+              </h3>
+              <div class="box-tools pull-right"></div>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
 
-      <!--------------------------
-        | Your Page Content Here |
-        -------------------------->
+              <div class="panel-body table-responsive" id="listadoRegistros">
+                <table id="tblListado" class="table table-stripped table-bordered table-condensed table-hover">
+                  <thead>
+                    <th>Opciones</th>
+                    <th>Nombre</th>
+                    <th>Descripción</th>
+                    <th>Estado</th>
+                  </thead>
+                  <tbody>
 
+                  </tbody>
+                  <tfoot>
+                    <th>Opciones</th>
+                    <th>Nombre</th>
+                    <th>Descripción</th>
+                    <th>Estado</th>
+                  </tfoot>
+                </table>
+              </div>
+
+              <div class="panel-body" id="formularioRegistros">
+                <p>Aquí va el formulario</p>
+              </div>
+
+            </div>
+            <!-- /.box-body -->
+
+          </div>
+        </div>
+      </div>
     </section>
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
-<?php include_once APP_PATH.'/Views/footer.php' ?>
+
+<?php include_once APP_PATH . '/Views/prefooter.php' ?>
+
+<!-- CUSTOM PAGE SCRIPTS FOOTER -->
+<script src="scripts/categoria.js"></script>
+
+<!-- Optionally, you can add Slimscroll and FastClick plugins.
+     Both of these plugins are recommended to enhance the
+     user experience. -->
+</body>
+</html>
