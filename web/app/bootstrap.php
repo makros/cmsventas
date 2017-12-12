563351dee61d82a14fb6a13c47a53b086047cd05
makros <lorente.marcos@gmail.com>
2017-12-12 12:16:44 +0100

    Se separa la lógica de controlador + accion al archivo routes

--- web/app/bootstrap.php
+++ web/app/bootstrap.php
@@ -1,18 +1,9 @@
 <?php
 
   require 'config/config.php';
-
-  if (isset($_GET['controller']) && isset($_GET['action'])) {
-    $controller = $_GET['controller'];
-    $action = $_GET['action'];
-  } else {
-    $controller = 'app';
-    $action = 'index';
-  }
-
-  require_once APP_PATH.'/routes.php';
-  require_once APP_PATH.'/Views/layout.php';
   require_once APP_PATH.'/db_conexion.php';
+  require_once APP_PATH.'/routes.php';
+
 
 
 
--- web/app/src/routes.php
+++ web/app/src/routes.php
@@ -1,35 +1,45 @@
 <?php
 
-  //Función que llama al controlador y su respectiva acción, que son pasados como parámetros
-use src\Controller\appController;
+  use src\Controller\appController;
 
-function call($controller, $action){
-    //importa el controlador desde la carpeta Controllers
-    require_once('Controller/' . $controller . 'Controller.php');
-    //Crea el controlador
-    switch($controller){
-      case 'app':
-        $controller = new appController();
-        break;
+  // Default Controller an action
+  $controller = 'app';
+  $action = 'index';
 
-    }
-    //llama a la acción del controlador
-    $controller->{$action }();
+  if (isset($_GET['controller']) && isset($_GET['action'])) {
+    $controller = $_GET['controller'];
+    $action = $_GET['action'];
   }
 
-  //Array con los controladores y sus respectivas acciones.
+  /* **DEFINICIÓN DE CONTROLADORES Y ACCIONES DEL PROYECTO** */
   $controllers = [
     'app'=>['index']
   ];
-  //Verifica que el controlador enviado desde index.php esté dentro del array controllers
-  if (array_key_exists($controller, $controllers)) {
-    //Verifica que el array controllers con la clave que es la variable controller del index exista la acción
-    if (in_array($action, $controllers[$controller])) {
-      //Llama  la función call y le pasa el controlador a llamar y la acción (método) que está dentro del controlador
-      call($controller, $action);
-    }else{
+  /* **FIN DEFINICIÓN DE CONTROLADORES Y ACCIONES DEL PROYECTO** */
+
+  function call($controller, $action){
+      //importa el controlador desde la carpeta Controllers
+      require_once('Controller/' . $controller . 'Controller.php');
+
+      switch($controller){
+        case 'app':
+          $controller = new appController();
+          break;
+
+      }
+      //Llamada a la acción del controlador
+      $controller->{$action}();
+    }
+
+    //Verifica que el controlador enviado desde index.php esté dentro del array controllers
+    if (array_key_exists($controller, $controllers)) {
+      //Verifica que el array controllers con la clave que es la variable controller del index exista la acción
+      if (in_array($action, $controllers[$controller])) {
+        //Llama  la función call y le pasa el controlador a llamar y la acción (método) que está dentro del controlador
+        call($controller, $action);
+      }else{
+        call('app', 'error');
+      }
+    }else{// le pasa el nombre del controlador y la pagina de error
       call('app', 'error');
     }
-  }else{// le pasa el nombre del controlador y la pagina de error
-    call('app', 'error');
-  }
